a60ca3791388f4d9d1c777e80db80927b52a43e6
Daniel Hidalgo <danielhidalgo@daniels-mbp.lan>
2020-05-07 08:19:40 -0500

    pull in post dynamically

--- includes/db.php
+++ includes/db.php
@@ -3,7 +3,7 @@
 $db["db_host"] = "localhost";
 $db["db_user"] = "root";
 $db["db_pass"] = "root";
-$db["db_name"] = "cms";
+$db["db_name"] = "php-cms";
 
 foreach($db as $key => $value) {
   define(strtoupper($key), $value);
--- index.php
+++ index.php
@@ -1,7 +1,5 @@
- <!-- Header -->
+<?php include "includes/db.php" ?>
 <?php include "includes/header.php" ?>
-
- <!-- Navigation -->
 <?php include "includes/nav.php" ?>
 
 
@@ -12,38 +10,60 @@
 
             <!-- Blog Entries Column -->
             <div class="col-md-8">
+            <?php
 
-                <h1 class="page-header">
+                $query = "SELECT * FROM posts";
+                $select_posts = mysqli_query($connection, $query);
+
+                while($row = mysqli_fetch_assoc($select_posts)) {
+                    $post_title = $row['post_title'];
+                    $post_author = $row['post_author'];
+                    $post_date = $row['post_date'];
+                    $post_image = $row['post_image'];
+                    $post_content = $row['post_content'];
+
+                ?>
+                
+                    <h1 class="page-header">
                     Page Heading
                     <small>Secondary Text</small>
                 </h1>
 
                 <!-- Blog Post -->
+                
                 <h2>
-                    <a href="#">Blog Post Title</a>
+                    <a href="#">
+                    <?php echo $post_title; ?>
+                    </a>
                 </h2>
                 <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
+                    by <a href="index.php">
+                    <?php echo $post_author; ?>
+                    </a>
                 </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
+                <p><span class="glyphicon glyphicon-time"></span> Posted on 
+                <?php echo $post_date; ?>
+                </p>
                 <hr>
                 <img class="img-responsive" src="http://placehold.it/900x300" alt="">
                 <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
+                <p>
+                <?php echo $post_content; ?>
+                </p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
+            <?php } ?>
 
             </div>
 
             <!-- Blog Sidebar Widgets Column -->
             <?php include "includes/sidebar.php" ?>
-           
 
         </div>
         <!-- /.row -->
 
         <hr>
- <!-- Footer -->
+<!-- Footer -->
 <?php include "includes/footer.php" ?>
 
