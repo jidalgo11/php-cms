b1ac237593cd1c5432303d026644fb795ac09982
Daniel Hidalgo <dhidalgo@dealerinspire.com>
2020-05-07 15:51:07 -0500

    added bg images and working on search functionality

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,17 +1,38 @@
 <div class="col-md-4">
 
+<?php 
+    if(isset($_POST['submit'])) {
+        $search = $_POST['search'];
+
+        $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%'";
+        $search_query = mysqli_query($connection, $query);
+
+        if(!$search_query) {
+            die("QUERY FAILED " . mysqli_error($connection));
+        }
+
+        $count = mysqli_num_rows($search_query);
+
+        if ($count == 0) {
+            echo "<span style='color: red;'>No Result</span>";
+        }
+    }
+?>
+
 <!-- Blog Search Well -->
 <div class="well">
     <h4>Blog Search</h4>
+    <form action="" method="post">
     <div class="input-group">
-        <input type="text" class="form-control">
+        <input type="text" name="search" class="form-control">
         <span class="input-group-btn">
-            <button class="btn btn-default" type="button">
+            <button name="submit" class="btn btn-default" type="submit">
                 <span class="glyphicon glyphicon-search"></span>
         </button>
         </span>
     </div>
     <!-- /.input-group -->
+    </form>
 </div>
 
 <!-- Blog Categories Well -->
